fdf82849236bb8eaee3081bd1ee994b7af529845
Damaera <lutfi.damaera@sebangsa.com>
2018-04-03 16:51:00 +0700

    some icon

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -4,179 +4,189 @@ import { Value } from "slate";
 import { Editor } from "slate-react";
 
 import AutoReplace from "slate-auto-replace";
-import SoftBreak from "slate-soft-break"
+import SoftBreak from "slate-soft-break";
 
-import serializer from './serializer'
-import initialValue from './initialValue.json'
+import serializer from "./serializer";
+import initialValue from "./initialValue.json";
 
-import hotKey from './plugins/hotKey';
-import normalize from './plugins/normalize';
+import hotKey from "./plugins/hotKey";
+import normalize from "./plugins/normalize";
 // import softBreak from './plugins/softBreak';
 // import link from './plugins/link';
+
+import BoldIcon from "../Icons/BoldIcon";
+import ItalicIcon from "../Icons/ItalicIcon";
 
 // import isUrl from 'is-url'
 
 export default class EditorApp extends React.Component {
-  
   state = {
-    value: Value.fromJSON(initialValue),
-  }
+    value: Value.fromJSON(initialValue)
+  };
 
   plugins = [
     normalize(this.state.value),
-    hotKey({ key: 'b', mark: true, type: 'bold' }),
-    hotKey({ key: '`', mark: true, type: 'code' }),
-    hotKey({ key: 'i', mark: true, type: 'italic' }),
-    hotKey({ key: '~', mark: true, type: 'strikethrough' }),
-    hotKey({ key: 'u', mark: true, type: 'underline' }),
-    hotKey({ key: '1', node: true, type: 'code-block' }),
+    hotKey({ key: "b", mark: true, type: "bold" }),
+    hotKey({ key: "`", mark: true, type: "code" }),
+    hotKey({ key: "i", mark: true, type: "italic" }),
+    hotKey({ key: "~", mark: true, type: "strikethrough" }),
+    hotKey({ key: "u", mark: true, type: "underline" }),
+    hotKey({ key: "1", node: true, type: "code-block" }),
     AutoReplace({
-      trigger: 'space', before: /^(>)$/,
-      transform: (transform, e, matches) => transform.setBlocks({ type: 'block-quote' })
+      trigger: "space",
+      before: /^(>)$/,
+      transform: (transform, e, matches) =>
+        transform.setBlocks({ type: "block-quote" })
     }),
     AutoReplace({
-      trigger: 'space', before: /^(#{1,3})$/,
+      trigger: "space",
+      before: /^(#{1,3})$/,
       transform: (transform, e, matches) => {
-        const { length } = matches.before[0]
-        let type = 'heading-one'
-        if (length === 2) { type = 'heading-two' }
-        else if (length === 3) { type = 'heading-three' }
-        return transform.setBlocks({ type })
+        const { length } = matches.before[0];
+        let type = "heading-one";
+        if (length === 2) {
+          type = "heading-two";
+        } else if (length === 3) {
+          type = "heading-three";
+        }
+        return transform.setBlocks({ type });
       }
     }),
     AutoReplace({
-      trigger: 'space', before: /^(-|\*|\+)$/,
-      transform: (transform, e, matches) => {        
+      trigger: "space",
+      before: /^(-|\*|\+)$/,
+      transform: (transform, e, matches) => {
         return transform
-        .setBlocks({ type: 'bulleted-item' })
-        .wrapBlock({ type: 'bulleted-list' })
+          .setBlocks({ type: "bulleted-item" })
+          .wrapBlock({ type: "bulleted-list" });
       }
     }),
     AutoReplace({
-      trigger: 'space', before: /^(1.)$/,
+      trigger: "space",
+      before: /^(1.)$/,
       transform: (transform, e, matches) => {
         return transform
-        .setBlock({ type: 'numbered-item' })
-        .wrapBlock({ type: 'numbered-list' })
+          .setBlock({ type: "numbered-item" })
+          .wrapBlock({ type: "numbered-list" });
       }
     }),
     AutoReplace({
-      trigger: 'enter', before: /^(---)$/,
+      trigger: "enter",
+      before: /^(---)$/,
       transform: (transform, e, matches) => {
         return transform
-        .setBlock({ type: 'divider', isVoid: true })
-        .insertBlock({ type: 'paragraph' })
+          .setBlock({ type: "divider", isVoid: true })
+          .insertBlock({ type: "paragraph" });
       }
     }),
     AutoReplace({
-      trigger: 'enter', before: /^(```)$/,
+      trigger: "enter",
+      before: /^(```)$/,
       transform: (transform, e, matches) => {
-        return transform
-        .setBlock({ type: 'code-block' })
+        return transform.setBlock({ type: "code-block" });
       }
     }),
     // markdownShortcut(),
     // softBreak(),
     // link()
-    SoftBreak({ shift: true }),
-  ]
-  
+    SoftBreak({ shift: true })
+  ];
+
   hasMark = type => {
-    const { value } = this.state
-    return value.activeMarks.some(mark => mark.type == type)
-  }
-  
+    const { value } = this.state;
+    return value.activeMarks.some(mark => mark.type == type);
+  };
+
   hasBlock = type => {
-    const { value } = this.state
-    return value.blocks.some(node => node.type == type)
-  }
-   
+    const { value } = this.state;
+    return value.blocks.some(node => node.type == type);
+  };
   onChange = ({ value }) => {
-    this.setState({ value })
-  }
-  
+    this.setState({ value });
+  };
+
   onClickMark = (e, type) => {
-    e.preventDefault()
-    const { value } = this.state
-    const change = value.change().toggleMark(type)
-    this.onChange(change)
-  }
+    e.preventDefault();
+    const { value } = this.state;
+    const change = value.change().toggleMark(type);
+    this.onChange(change);
+  };
 
   onClickBlock = (event, type) => {
-    event.preventDefault()
-    const { value } = this.state
-    const change = value.change()
-    const { document } = value
+    event.preventDefault();
+    const { value } = this.state;
+    const change = value.change();
+    const { document } = value;
 
     // Handle everything but list buttons.
-    if (type != 'bulleted-list' && type != 'numbered-list') {
-      const isActive = this.hasBlock(type)
-      const isList = this.hasBlock('list-item')
+    if (type != "bulleted-list" && type != "numbered-list") {
+      const isActive = this.hasBlock(type);
+      const isList = this.hasBlock("list-item");
 
       if (isList) {
         change
-          .setBlocks(isActive ? 'paragraph' : type)
-          .unwrapBlock('bulleted-list')
-          .unwrapBlock('numbered-list')
+          .setBlocks(isActive ? "paragraph" : type)
+          .unwrapBlock("bulleted-list")
+          .unwrapBlock("numbered-list");
       } else {
-        change.setBlocks(isActive ? 'paragraph' : type)
+        change.setBlocks(isActive ? "paragraph" : type);
       }
     } else {
       // Handle the extra wrapping required for list buttons.
-      const isList = this.hasBlock('list-item')
+      const isList = this.hasBlock("list-item");
       const isType = value.blocks.some(block => {
-        return !!document.getClosest(block.key, parent => parent.type == type)
-      })
+        return !!document.getClosest(block.key, parent => parent.type == type);
+      });
 
       if (isList && isType) {
         change
-          .setBlocks('paragraph')
-          .unwrapBlock('bulleted-list')
-          .unwrapBlock('numbered-list')
+          .setBlocks("paragraph")
+          .unwrapBlock("bulleted-list")
+          .unwrapBlock("numbered-list");
       } else if (isList) {
         change
           .unwrapBlock(
-            type == 'bulleted-list' ? 'numbered-list' : 'bulleted-list'
+            type == "bulleted-list" ? "numbered-list" : "bulleted-list"
           )
-          .wrapBlock(type)
+          .wrapBlock(type);
       } else {
-        change.setBlocks('list-item').wrapBlock(type)
+        change.setBlocks("list-item").wrapBlock(type);
       }
     }
 
-    this.onChange(change)
-  }
+    this.onChange(change);
+  };
 
   onSerialize = () => {
     // const { nodes } = this.state.value.toJSON().document
-    console.log(serializer.serialize(this.state.value))
-  }
+    console.log(serializer.serialize(this.state.value));
+  };
 
   onClickLink = e => {
-    e.preventDefault()
-    const { value } = this.state
+    e.preventDefault();
+    const { value } = this.state;
     const hasLinks = value.inlines.some(inline => inline.type == "link");
-    const change = value.change()
+    const change = value.change();
 
     if (hasLinks) {
       change.unwrapInline("link");
     } else if (value.isExpanded) {
-      const href = window.prompt('Enter the URL of the link:')
+      const href = window.prompt("Enter the URL of the link:");
       change.wrapInline({ type: "link", data: { href } });
       change.collapseToEnd();
-    // } else {
-    //   const href = window.prompt('Enter the URL of the link:')
-    //   const text = window.prompt('Enter the text for the link:')
-    //   change
-    //     .insertText(text)
-    //     .extend(0 - text.length)
-    //     .wrapInline({ type: "link", data: { href } });
+      // } else {
+      //   const href = window.prompt('Enter the URL of the link:')
+      //   const text = window.prompt('Enter the text for the link:')
+      //   change
+      //     .insertText(text)
+      //     .extend(0 - text.length)
+      //     .wrapInline({ type: "link", data: { href } });
     }
 
-    this.onChange(change)
-  }
+    this.onChange(change);
+  };
 
-  renderNode = (props) => {
+  renderNode = props => {
     switch (props.node.type) {
       case "code-block":
         return <pre {...props.attributes}>{props.children}</pre>;
@@ -207,14 +217,18 @@ export default class EditorApp extends React.Component {
       case "link": {
         const { data } = props.node;
         const href = data.get("href");
-        return <a {...props.attributes} href={href}>{props.children}</a>;
+        return (
+          <a {...props.attributes} href={href}>
+            {props.children}
+          </a>
+        );
       }
       case "divider":
         return <hr />;
     }
-  }
+  };
 
-  renderMark = (props) => {
+  renderMark = props => {
     switch (props.mark.type) {
       case "bold":
         return <strong>{props.children}</strong>;
@@ -227,77 +241,88 @@ export default class EditorApp extends React.Component {
       case "underline":
         return <u>{props.children}</u>;
     }
-  }
+  };
 
   renderToolbar = () => {
     return (
-      <div>
+      <div className="toolbar">
+        <div className="toolbar-group">
+          <button
+            className={this.hasMark("bold") ? "active" : ""}
+            onMouseDown={e => this.onClickMark(e, "bold")}
+          >
+            <BoldIcon />
+          </button>
+          <button
+            className={this.hasMark("italic") ? "active" : ""}
+            onMouseDown={e => this.onClickMark(e, "italic")}
+          >
+            <ItalicIcon />
+          </button>
+          <button
+            className={this.hasMark("underline") ? "active" : ""}
+            onMouseDown={e => this.onClickMark(e, "underline")}
+          >
+            <u>u</u>
+          </button>
+        </div>
+        <div className="toolbar-group">
+          <button
+            className={this.hasMark("code") ? "active" : ""}
+            onMouseDown={e => this.onClickMark(e, "code")}
+          >
+            <code>code</code>
+          </button>
+          <button
+            className={
+              this.state.value.inlines.some(inline => inline.type == "link")
+                ? "active"
+                : ""
+            }
+            onMouseDown={this.onClickLink}
+          >
+            link
+          </button>
+        </div>
         <button
-          className={this.hasMark('bold') ? 'active' : ''}
-          onMouseDown={e => this.onClickMark(e, "bold")}>
-          <b>B</b>
-        </button>
-        <button
-          className={this.hasMark('italic') ? 'active' : ''}
-          onMouseDown={e => this.onClickMark(e, "italic")}>
-          <i>i</i>
-        </button>
-        <button
-          className={this.hasMark('underline') ? 'active' : ''}
-          onMouseDown={e => this.onClickMark(e, "underline")}>
-          <u>u</u>
-        </button>
-        <button
-          className={this.hasMark('code') ? 'active' : ''}
-          onMouseDown={e => this.onClickMark(e, "code")}>
-          <code>code</code>
-        </button>
-        <button
-          className={
-            this.state.value.inlines.some(
-              inline => inline.type == 'link'
-            ) ? 'active' : ''
-          }
-          onMouseDown={this.onClickLink}>
-          link
-        </button>
-        <button
-          className={this.hasBlock('heading-one') ? 'active' : ''}
-          onMouseDown={e => this.onClickBlock(e, "heading-one")}>
+          className={this.hasBlock("heading-one") ? "active" : ""}
+          onMouseDown={e => this.onClickBlock(e, "heading-one")}
+        >
           H1
         </button>
         <button
-          className={this.hasBlock('heading-two') ? 'active' : ''}
-          onMouseDown={e => this.onClickBlock(e, "heading-two")}>
+          className={this.hasBlock("heading-two") ? "active" : ""}
+          onMouseDown={e => this.onClickBlock(e, "heading-two")}
+        >
           H2
         </button>
         <button
-          className={(
-            this.hasBlock('heading-three') ||
-            this.hasBlock('heading-four') ||
-            this.hasBlock('heading-five') ||
-            this.hasBlock('heading-six')
-          ) ? 'active' : ''}
-          onMouseDown={e => this.onClickBlock(e, "heading-three")}>
+          className={
+            this.hasBlock("heading-three") ||
+            this.hasBlock("heading-four") ||
+            this.hasBlock("heading-five") ||
+            this.hasBlock("heading-six")
+              ? "active"
+              : ""
+          }
+          onMouseDown={e => this.onClickBlock(e, "heading-three")}
+        >
           H3
         </button>
         <button
-          className={this.hasBlock('block-quote') ? 'active' : ''}
-          onMouseDown={e => this.onClickBlock(e, "block-quote")}>
+          className={this.hasBlock("block-quote") ? "active" : ""}
+          onMouseDown={e => this.onClickBlock(e, "block-quote")}
+        >
           quote
         </button>
-        <button
-          onMouseDown={this.onSerialize}>
-          serialize
-        </button>
+        <button onMouseDown={this.onSerialize}>serialize</button>
       </div>
-    )
-  }
- 
+    );
+  };
   render() {
     return (
       <div>
-        { this.renderToolbar() }
+        {this.renderToolbar()}
         <Editor
           plugins={this.plugins}
           value={this.state.value}
@@ -305,8 +330,7 @@ export default class EditorApp extends React.Component {
           renderNode={this.renderNode}
           renderMark={this.renderMark}
         />
-
       </div>
-    )
+    );
   }
 }
