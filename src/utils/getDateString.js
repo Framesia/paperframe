ebdd35203a988d7d7cf262ef3e3208f33cdfba05
damaera <dmrbuu@gmail.com>
2018-05-08 21:24:43 +0700

    delete console

--- src/utils/getDateString.js
+++ src/utils/getDateString.js
@@ -27,7 +27,6 @@ const getDateString = date => {
   const day = date.getDate();
   const monthIndex = date.getMonth();
   const year = date.getFullYear();
-  console.log(date);
 
   const now = new Date();
   if (
